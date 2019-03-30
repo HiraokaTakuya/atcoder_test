00fb4360ed443a3c76c0e33f092616f6986406c2
HiraokaTakuya <hiraoka64@gmail.com>
2019-03-30 18:36:36 +0900

    add some error checks

--- src/main.rs
+++ src/main.rs
@@ -37,13 +37,21 @@ impl Engine {
 
 fn main() {
     let args = std::env::args().collect::<Vec<_>>(); // target contest problem
+    if args.len() != 3 {
+        eprintln!("arguments errror!\nexpected: {} <contest name (for example: abc121)> <problem name (for example: a)>", args[0]);
+        return;
+    }
     let inouts = {
-        let mut res = reqwest::get(&format!(
+        let url = format!(
             "https://atcoder.jp/contests/{0}/tasks/{0}_{1}",
             args[1], args[2]
-        ))
-        .unwrap();
+        );
+        let mut res = reqwest::get(&url).expect(&format!("Can't get from {}", url));
         let body = res.text().unwrap();
+        if body.contains(r"<title>404 Not Found - AtCoder</title>") {
+            eprintln!("The URL is not found.\nURL: {}", url);
+            return;
+        }
         let mut sample_count = 1; // 1 origin.
         enum State {
             Input,
@@ -91,12 +99,14 @@ fn main() {
         }
         inouts
     };
-    Command::new("cargo")
+    let status = Command::new("cargo")
         .args(&["build", "--bin", &args[2]])
-        .spawn()
-        .unwrap()
-        .wait()
+        .status()
         .unwrap();
+
+    if !status.success() {
+        return;
+    }
 
     for (idx, inout) in inouts.iter().enumerate() {
         let mut engine = Engine::new(&format!("target/debug/{}", args[2]));
